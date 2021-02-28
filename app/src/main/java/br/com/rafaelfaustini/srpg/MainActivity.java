83ab37f2c4e256925306f17321f33070cd85f2a7
Rafael Faustini <contato@rafaelfaustini.com.br>
2021-02-28 00:59:23 -0300

    Simple dice roll

--- app/src/main/java/br/com/rafaelfaustini/srpg/MainActivity.java
+++ app/src/main/java/br/com/rafaelfaustini/srpg/MainActivity.java
@@ -3,12 +3,34 @@ package br.com.rafaelfaustini.srpg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import br.com.rafaelfaustini.srpg.model.Dice;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void roll(View view){
+        try {
+            EditText editSides = findViewById(R.id.editDiceSides);
+            int faces = Integer.parseInt(editSides.getText().toString());
+            Dice dice = new Dice(faces);
+            TextView result = findViewById(R.id.diceResult);
+            String resultText = String.valueOf(dice.roll());
+            result.setText(resultText);
+            result.setVisibility(1);
+        } catch (Exception e){
+
+        }
+
     }
 }
---
+++ app/src/main/java/br/com/rafaelfaustini/srpg/model/Dice.java
@@ -0,0 +1,26 @@
+package br.com.rafaelfaustini.srpg.model;
+import java.lang.Math;
+
+public class Dice {
+    private int faces;
+
+    public Dice(int faces) {
+        this.faces = faces;
+    }
+
+    public int getFaces() {
+        return faces;
+    }
+
+    public void setFaces(int faces) {
+        this.faces = faces;
+    }
+
+    private int random(int max){
+        return (int) (Math.random()*(max-1+1)+1);
+    }
+
+    public int roll(){
+        return random(faces);
+    }
+}
